5bdb63fac16a85f0420019370e2dcd850c0ec987
Afreen <66796684+Shaika07@users.noreply.github.com>
2022-11-08 21:19:38 +0530

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,13 @@
+#i
+
+
+from random_word import RandomWords
+from quote import quote
+r = RandomWords()
+w = r.get_random_word()
+print("KeyWord Generated: ",w)
+res = quote(w,limit=1)
+for i in range(len(res)):
+    print("\nQuote Generated: ",res[i]['quote'])
+
+
